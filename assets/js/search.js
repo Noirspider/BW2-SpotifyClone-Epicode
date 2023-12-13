7c751006da5faca5dfa201f3d25cefe33bc3353a
Noirspider <83585087+Noirspider@users.noreply.github.com>
2023-12-13 14:13:31 +0100

    aggiunta token ed impostazione file search

---
+++ assets/js/search.js
@@ -0,0 +1,14 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-bar");
+
+const searchContainer = document.getElementById("search-container");
+
+const searchURL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
+// const artistURL = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+
+const token = "18ec06a3e2mshc92412178be162ap1a604djsn2c54c747cb17"; // Token Gian
+// const token = "940dce657bmshd8c731d3dbd3502p18a658jsn330fcc56530d"  // Token Jag
+// const token = "66b19f990dmsh7c4ff58d5ea21d0p1aa92fjsnd50246c25f1d"  // Token Val
+// const token = "c4234d51b0msh31724428db3a8bfp114b28jsnf03bb1f920a7"  // Token Dav
+
+const Host = "deezerdevs-deezer.p.rapidapi.com";
