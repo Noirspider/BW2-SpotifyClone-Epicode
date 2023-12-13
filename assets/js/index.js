625202c0a9eb7a48fb75a90e344736cb342b990a
ValCas18 <valeriocasadei18@gmail.com>
2023-12-13 17:01:24 +0100

    commit 17

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,103 +1,100 @@
 const playlists = [
-  {
-    img: "./assets/imgs/main/image-1.jpg",
-    name: "Giù",
-    artist: "Neima Ezza"
-  },
-  {
-    img: "./assets/imgs/main/image-2.jpg",
-    name: "Pov: la tua estate è un bel film",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-5.jpg",
-    name: "Lo-fi Beats",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-6.jpg",
-    name: "Jazz Vibes",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-7.jpg",
-    name: "Caffè del buongiorno",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-8.jpg",
-    name: "Sangue Giovane",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-9.jpg",
-    name: "Canta Sotto La Doccia",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-17.jpg",
-    name: "New Music Friday",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-18.jpg",
-    name: "Estate 2023",
-    artist: "Spotify"
-  },
-  {
-    img: "./assets/imgs/main/image-19.jpg",
-    name: "Street Culto",
-    artist: "Spotify"
-  }
+	{
+		img: "./assets/imgs/main/image-1.jpg",
+		name: "Hanno Ucciso Pippo Ragno",
+		artist: "PippoPippo3",
+	},
+	{
+		img: "./assets/imgs/main/image-2.jpg",
+		name: "Pippo Rock",
+		artist: "System of a Pippo",
+	},
+	{
+		img: "./assets/imgs/main/image-5.jpg",
+		name: "Sweet Pippo of Mine",
+		artist: "Pippo 'n Roses",
+	},
+	{
+		img: "./assets/imgs/main/image-6.jpg",
+		name: "Sanam Lo-Fi",
+		artist: "23",
+	},
+	{
+		img: "./assets/imgs/main/image-7.jpg",
+		name: "Morning Pippo",
+		artist: "Giacomino",
+	},
+	{
+		img: "./assets/imgs/main/image-8.jpg",
+		name: "Punkabbestia e Pippo",
+		artist: "Sanam",
+	},
+	{
+		img: "./assets/imgs/main/image-9.jpg",
+		name: "POV: Pippo e la tua Ragazza",
+		artist: "Pop Pippo",
+	},
+	{
+		img: "./assets/imgs/main/image-17.jpg",
+		name: "Quel Pippo, Quel Maledetto Pippo",
+		artist: "Pippofy",
+	},
+	{
+		img: "./assets/imgs/main/image-18.jpg",
+		name: "EMMOBASTAPIPPO",
+		artist: "Stefano Miceli",
+	},
+	{
+		img: "./assets/imgs/main/image-19.jpg",
+		name: "Per un Pugno di Pippo",
+		artist: "Noi",
+	},
 ];
 
 function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
+	let currentIndex = array.length,
+		randomIndex;
 
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+	// While there remain elements to shuffle.
+	while (currentIndex > 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex]
-    ];
-  }
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
 
-  return array;
+	return array;
 }
 
-const getCorrectGreeting = (placeholderHtml) => {
-  const date = new Date();
-  const time = date.getHours();
+const getGreeting = (placeholderHtml) => {
+	const date = new Date();
+	const time = date.getHours();
 
-  if (time < 12) {
-    placeholderHtml.innerText = "Buongiorno";
-  }
-  if (time > 12 && time < 18) {
-    placeholderHtml.innerText = "Buon Pomeriggio";
-  }
-  if (time > 18) {
-    placeholderHtml.innerText = "Buonasera";
-  }
+	if (time < 12) {
+		placeholderHtml.innerText = "Buongiorno";
+	}
+	if (time > 12 && time < 18) {
+		placeholderHtml.innerText = "Buon Pomeriggio";
+	}
+	if (time > 18) {
+		placeholderHtml.innerText = "Buonasera";
+	}
 };
 
 const loadInterestCards = () => {
-  const cardContainer = document.getElementById("interest-card-container");
+	const cardContainer = document.getElementById("interest-card-container");
 
-  const randomizedPlaylists = shuffle(playlists);
+	const randomizedPlaylists = shuffle(playlists);
 
-  randomizedPlaylists.forEach((playlist) => {
-    const cardCol = document.createElement("div");
-    cardCol.classList = "col-6 col-md-2";
-    const card = document.createElement("div");
-    card.classList = "w-100 card p-3 shadow mb-3 bg-dark-2 border-0 pointer";
-    // card.style = "width: calc(20% - 1rem)";
-    card.innerHTML = `
+	randomizedPlaylists.forEach((playlist) => {
+		const cardCol = document.createElement("div");
+		cardCol.classList = "col-6 col-md-2";
+		const card = document.createElement("div");
+		card.classList = "w-100 card p-3 shadow mb-3 bg-dark-2 border-0 pointer";
+		// card.style = "width: calc(20% - 1rem)";
+		card.innerHTML = `
     <img
       src="${playlist.img}"
       class="card-img-top"
@@ -109,13 +106,13 @@ const loadInterestCards = () => {
     </div>
     `;
 
-    cardCol.appendChild(card);
-    cardContainer.appendChild(cardCol);
-  });
+		cardCol.appendChild(card);
+		cardContainer.appendChild(cardCol);
+	});
 };
 
 window.onload = () => {
-  const greetingPlaceholder = document.getElementById("time-greeting");
-  getCorrectGreeting(greetingPlaceholder);
-  loadInterestCards();
+	const greetingPlaceholder = document.getElementById("time-greeting");
+	getGreeting(greetingPlaceholder);
+	loadInterestCards();
 };
--- assets/js/search.js
+++ assets/js/search.js
@@ -4,7 +4,6 @@ const searchInput = document.getElementById("search-bar");
 const searchContainer = document.getElementById("search-container");
 
 const searchURL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
-// const artistURL = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 
 const token = "18ec06a3e2mshc92412178be162ap1a604djsn2c54c747cb17"; // Token Gian
 // const token = "940dce657bmshd8c731d3dbd3502p18a658jsn330fcc56530d"  // Token Jag
@@ -12,3 +11,41 @@ const token = "18ec06a3e2mshc92412178be162ap1a604djsn2c54c747cb17"; // Token Gia
 // const token = "c4234d51b0msh31724428db3a8bfp114b28jsnf03bb1f920a7"  // Token Dav
 
 const Host = "deezerdevs-deezer.p.rapidapi.com";
+
+const searcArtist = async function () {
+	try {
+		const response = await fetch(searchURL + searchInput.value, {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key": token,
+				"X-RapidAPI-Host": Host,
+			},
+		});
+		if (!response.ok) {
+			throw new Error("NON ERANO QUESTI GLI ACCORDI!");
+		}
+		const responseObj = await response.json();
+		console.log(responseObj);
+
+		const allAlbums = [];
+		searchContainer.innerHTML = "";
+		for (let i = 0; i < responseObj.data.lenght; i++) {
+			const element = responseObj.data[i];
+			if (
+				element.artist.name
+					.toLowerCase()
+					.normalize("NFD")
+					.replace(/[\u0300-\u036f]/g, "")
+					.includes(
+						searchInput.value
+							.toLowerCase()
+							.normalize("NFD")
+							.replace(/[\u0300-\u036f]/g, "")
+					)
+			) {
+				const artist = element.artist;
+				const artistId = element.artist.id;
+			}
+		}
+	} catch (error) {}
+};
